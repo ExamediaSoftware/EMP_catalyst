80e47ed64bdc33b1f1f808d19894b3700fd9a360
Afdhaluddin <awakenblueheart@gmail.com>
2022-05-26 16:26:29 +0800

    Blade - Layout
    Layout for application

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,9 +1,11 @@
-<x-app-layout>
-    <x-slot name="header">
+@extends('layouts.app')
+@section('content')
+{{-- <x-app-layout> --}}
+    {{-- <x-slot name="header"> --}}
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Profile') }}
         </h2>
-    </x-slot>
+    {{-- </x-slot> --}}
 
     <div>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
@@ -42,4 +44,5 @@
             @endif
         </div>
     </div>
-</x-app-layout>
+{{-- </x-app-layout> --}}
+@endsection
