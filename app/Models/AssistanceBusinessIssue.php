d54b232aa731d323d64e1f91228417639370ec9b
Afdhaluddin <awakenblueheart@gmail.com>
2022-05-26 16:29:28 +0800

    Models

---
+++ app/Models/AssistanceBusinessIssue.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AssistanceBusinessIssue extends Model
+{
+    use HasFactory;
+}
